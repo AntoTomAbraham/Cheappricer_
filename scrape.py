d974a8d18ba61322723c6a56529546304d726ec7
Andrew George Issac <andrewgeorgeissac@gmail.com>
2021-07-25 16:27:13 +0530

    main

--- scrape.py
+++ scrape.py
@@ -1,1 +1,22 @@
-from bs4 import 
+from bs4 import BeautifulSoup
+import requests,datetime,json,time
+
+
+
+def amazon_india(url):
+    pass
+
+
+def flikart(url):
+    pass
+
+
+def croma(url):
+    pass
+
+
+def rel_digital(url):
+    pass
+
+def tata_clk(url):
+    pass
