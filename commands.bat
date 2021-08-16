3c68fdfc8d70215109f2db39e8aa0852263c5227
cheappricer <cheappricer@gmail.com>
2021-08-16 06:47:10 +0000

    commit_name

--- commands.bat
+++ commands.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 git remote set-url origin https://cheappricer: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@github.com/cheappricer/Cheappricer_.git
-git pull origin master 
+git pull origin main 
 git config --global user.email "cheappricer@gmail.com"
 git config --global user.name "cheappricer"
 git add .
