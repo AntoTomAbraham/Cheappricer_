0076d97f08a2374e583d4f2051cda7d06eaef900
cheappricer <cheappricer@gmail.com>
2021-08-17 04:27:53 +0000

    AzureCommit

--- commands.bat
+++ commands.bat
@@ -4,5 +4,5 @@ git pull origin main
 git config --global user.email "cheappricer@gmail.com"
 git config --global user.name "cheappricer"
 git add .
-git commit -m "commit_name"
+git commit -m "AzureCommit"
 git push -u origin main
