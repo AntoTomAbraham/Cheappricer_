7551cdc53e5862a156a9f4f8407fbe672dcd2e38
Andrew George Issac <andrewgeorgeissac@gmail.com>
2021-09-03 15:23:29 +0530

    data entry

--- routes/api.js
+++ routes/api.js
@@ -9,12 +9,24 @@ router.use(bodyParser.json())
 const app=express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+//json
+var file=fs.readFileSync('JSON_Data/product_data.json'); //reading JSON --product_data.json
+var product_data=JSON.parse(file);
+var file1=fs.readFileSync('JSON_Data/price_data.json');
+var price_data=JSON.parse(file1);
 
 app.get("/",(req,res)=>{
     res.send("API")
 })
 
+//using productId
 app.get("/priceData",(req,res)=>{
-    res.send(req.query.proId)
+    res.send(price_data[req.query.proId])
+
 })
+
+
+
 module.exports=app;
+
+
